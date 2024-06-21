993c8c048596bbd9a5f80d5ef34a197ec960dd75
JfffV <ultionem14@gmail.com>
2024-06-21 14:57:29 +0200

    added voices and variant

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -1,4 +1,4 @@
-//task 1
+//task 1 +
 //Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
 function stringToArray(string) {
@@ -11,8 +11,18 @@ function stringToArray(string) {
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, "U");
 }
+//
+function DNAtoRNA(dna) {
+  let rna = dna.split("");
+  rna.forEach((letter, index) => {
+    if (letter === "T") {
+      rna[index] = "U";
+    }
+  });
+  return rna.join("");
+}
 
-//Task 3
+//Task 3 +
 //Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
 var min = function (list) {
@@ -23,11 +33,10 @@ var max = function (list) {
   return Math.max(...list);
 };
 
-//Task 4
+//Task 4 +
 //Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
   const minValue = Math.min(...arr);
   return toReturn === "value" ? minValue : arr.indexOf(minValue);
 }
-
