453cbbbb2c839608eb41f393104fa624aab13982
KhrystynaShvachko <145377374+KhrystynaShvachko@users.noreply.github.com>
2024-06-26 15:33:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function init() {
     import('./index.about-us.carousel.js');
     import('./contacts.distribution-map.js');
     import('./contacts.visit-us.js');
-    import('./about-us.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
